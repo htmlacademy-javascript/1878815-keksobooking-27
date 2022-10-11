679ef4d23eab299abca3d55bdc721fe69ad0de25
afamaria <marie.afanasyeva@gmail.com>
2022-10-11 14:46:15 +0300

    lint check

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ let digits;
 let lower = Math.min(min,max);
 let upper = Math.max(min,max);
 
-let getRandom = (lower>=0 && upper>=0)
+const getRandom = (lower >= 0 && upper >= 0)
   ? Math.floor(lower + Math.random() * (upper + 1 - lower))
-    : (upper=lower && upper>=0 && lower>=0)
-     ? Math.floor(upper)
-        : NaN;
+  : (upper = lower && upper >= 0 && lower >= 0)
+    ? Math.floor(upper)
+    : NaN;
 
-console.log(getRandom)
+console.log(getRandom);
 
-let getRandomFloat = (lower<0 || upper<0 || digits<0)
+const getRandomFloat = (lower < 0 || upper < 0 || digits < 0)
   ? NaN
-    : +(Math.random() * (upper - lower) + lower).toFixed(digits);
+  : +(Math.random() * (upper - lower) + lower).toFixed(digits);
 
-console.log(getRandomFloat)
+console.log(getRandomFloat);
