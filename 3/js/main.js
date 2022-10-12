5ef0ef459c7c280ac4a7720147ef765431528405
Keks <keks@htmlacademy.ru>
2022-10-13 02:12:20 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,19 +1,32 @@
-let min;
-let max;
-let digits;
-let lower = Math.min(min,max);
-let upper = Math.max(min,max);
+function getRandom(min, max) {
 
-const getRandom = (lower >= 0 && upper >= 0)
-  ? Math.floor(lower + Math.random() * (upper + 1 - lower))
-  : (upper = lower && upper >= 0 && lower >= 0)
-    ? Math.floor(upper)
-    : NaN;
+  if (min < 0 || max < 0 || typeof(min) !== 'number' || typeof(max) !== 'number') {
+    return NaN;
+  } else if (min >= 0 && max >= 0) {
+    const lower = Math.ceil(Math.min(min,max));
+    const upper = Math.floor(Math.max(min,max));
+    if (lower === upper) {
+      return lower;
+    } else if (lower > upper) {
+      return NaN;
+    } else {
+      return Math.floor(lower + Math.random() * (upper + 1 - lower));
+    }
+  } else {
+    return NaN;
+  }
+}
 
-console.log(getRandom);
+getRandom();
 
-const getRandomFloat = (lower < 0 || upper < 0 || digits < 0)
-  ? NaN
-  : +(Math.random() * (upper - lower) + lower).toFixed(digits);
+function getRandomFloat(min, max, digits) {
+  if (min < 0 || max < 0 || typeof(min) !== 'number' || typeof(max) !== 'number' || typeof(digits) !== 'number' || digits < 0 || digits > 100 || Math.ceil(digits) !== Math.floor(digits) ) {
+    return NaN;
+  } else {
+    const lower = Math.min(min,max);
+    const upper = Math.max(min,max);
+    return +(Math.random() * (upper - lower) + lower).toFixed(digits);
+  }
+}
 
-console.log(getRandomFloat);
+getRandomFloat();
